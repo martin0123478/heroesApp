749a70efff669d07790d62a627bf3f46d5ca3776
martin0123478 <martin_agui97@outlook.es>
2021-09-13 22:12:54 -0500

    creacion de modulos, componentes, router, lazy loading

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes =[
+  {
+    path:'',
+    children:[
+      {
+        path:'login',
+        component:LoginComponent
+      },
+      {
+        path:'registro',
+        component:RegisterComponent
+      },
+      {
+        path:'**',
+        component:LoginComponent
+      },
+      
+    ]
+  }
+]
+
+@NgModule({
+  declarations: [
+   
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AuthRoutingModule { }
