ea18aa970484883b1c2038f4677e857f8ab09072
martin0123478 <martin_agui97@outlook.es>
2021-09-22 23:42:11 -0500

    se crea variable de entorno del endpoint

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,18 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
-
+private baseUrl = environment.baseUrl
   constructor(private http:HttpClient) { }
 
   getHeroes(){
-   return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+   return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
   }
   getHeroe(id:string){
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`)
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`)
   }
 }
